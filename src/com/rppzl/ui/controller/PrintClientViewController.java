379ad4d59e66e1034faf3627e64f4493fa27db55
Siarhei Audzeichyk <xlebysheklol@gmail.com>
2020-09-13 21:20:43 +0300

    Implement Client view table

--- src/com/rppzl/ui/controller/PrintClientViewController.java
+++ src/com/rppzl/ui/controller/PrintClientViewController.java
@@ -1,18 +1,41 @@
 package com.rppzl.ui.controller;
 
+import com.rppzl.dao.ClientDAO;
+import com.rppzl.dao.DAO;
+import com.rppzl.entity.Client;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class PrintClientViewController {
+public class PrintClientViewController implements Initializable {
+
     @FXML private Button backToMenuButton;
+
+    @FXML private TableView<Client> tableView;
+    @FXML private TableColumn<Client, String> lastNameColumn;
+    @FXML private TableColumn<Client, String> firstNameColumn;
+    @FXML private TableColumn<Client, String> middleNameColumn;
+    @FXML private TableColumn<Client, LocalDate> dateOfBirthColumn;
+    @FXML private TableColumn<Client, String> passportSeriesColumn;
+    @FXML private TableColumn<Client, String> passportNumberColumn;
+    @FXML private TableColumn<Client, String> emailColumn;
 
 
     public void changeSceneToMainMenu(ActionEvent event) throws IOException
@@ -27,5 +50,24 @@ public class PrintClientViewController {
         window.setTitle("Главное меню");
         window.setScene(signupViewScene);
         window.show();
+    }
+
+    public ObservableList<Client> getClientObservableList() {
+        DAO<Client> dao = new ClientDAO();
+        List<Client> list = dao.getList();
+        return FXCollections.observableArrayList(list);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        middleNameColumn.setCellValueFactory(new PropertyValueFactory<>("middleName"));
+        dateOfBirthColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+        passportSeriesColumn.setCellValueFactory(new PropertyValueFactory<>("passportSeries"));
+        passportNumberColumn.setCellValueFactory(new PropertyValueFactory<>("passportNumber"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        tableView.setItems(getClientObservableList());
     }
 }
