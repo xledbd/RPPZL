7b01ceb74c5a9208f404e6108151c2d976051485
Siarhei Audzeichyk <xlebysheklol@gmail.com>
2020-09-14 00:00:53 +0300

    Add text fields to AddClientViewController

--- src/com/rppzl/ui/controller/AddClientViewController.java
+++ src/com/rppzl/ui/controller/AddClientViewController.java
@@ -1,18 +1,45 @@
 package com.rppzl.ui.controller;
 
+import com.rppzl.entity.City;
+import com.rppzl.entity.Country;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AddClientViewController {
+
     @FXML private Button backToMenuButton;
+
+    @FXML private TextField lastNameField;
+    @FXML private TextField firstNameField;
+    @FXML private TextField middleNameField;
+    @FXML private DatePicker dateOfBirthField;
+    @FXML private TextField passportSeriesField;
+    @FXML private TextField passportNumberField;
+    @FXML private TextField authorityField;
+    @FXML private DatePicker dateOfIssueField;
+    @FXML private TextField identificationNumberField;
+    @FXML private TextField placeOfBirthField;
+    @FXML private ChoiceBox<City> cityOfResidenceChoiceBox;
+    @FXML private TextField addressOfResidenceField;
+    @FXML private TextField landlinePhoneField;
+    @FXML private TextField mobilePhoneField;
+    @FXML private TextField emailField;
+    @FXML private TextField placeOfWorkField;
+    @FXML private TextField positionField;
+    @FXML private ChoiceBox<City> cityOfRegistrationChoiceBox;
+    @FXML private ChoiceBox<String> familyStatusChoiceBox;
+    @FXML private ChoiceBox<Country> citizenshipChoiceBox;
+    @FXML private ChoiceBox<String> disabilityChoiceBox;
+    @FXML private CheckBox retireeCheckBox;
+    @FXML private TextField monthlyIncomeField;
 
 
     public void changeSceneToMainMenu(ActionEvent event) throws IOException
