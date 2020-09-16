c8b1560ac7acc541a46fd5d0c2a15682ee787101
Anton <antonundrul@gmail.com>
2020-09-16 20:06:53 +0300

    added functionality to addCityButton and addCountryButton

--- src/com/rppzl/ui/controller/AddCityViewController.java
+++ src/com/rppzl/ui/controller/AddCityViewController.java
@@ -1,4 +1,30 @@
 package com.rppzl.ui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddCityViewController {
+
+
+    public void showAddCountryView(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("com/rppzl/ui/fxml/AddCountryView.fxml"));
+        Parent parent = loader.load();
+
+        Scene scene = new Scene(parent);
+        Stage popupStage = new Stage();
+
+        popupStage.initOwner(((Node)event.getSource()).getScene().getWindow());
+        popupStage.initModality(Modality.WINDOW_MODAL);
+        popupStage.setScene(scene);
+        popupStage.showAndWait();
+
+    }
 }
